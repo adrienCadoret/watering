a664563dbb732ee0269111b3f927474b11952514
adrien <adri.cadoret@gmail.com>
2018-12-30 22:27:24 +0000

    chore() : add print out + edit gitignore

--- water.py
+++ water.py
@@ -31,6 +31,7 @@ def auto_water(delay = 5, pump_pin = 7, water_sensor_pin = 8):
         while 1 and consecutive_water_count < 10:
             time.sleep(delay)
             wet = get_status(pin = water_sensor_pin) == 0
+            print(wet)
             if not wet:
                 if consecutive_water_count < 5:
                     pump_on(pump_pin, 1)
